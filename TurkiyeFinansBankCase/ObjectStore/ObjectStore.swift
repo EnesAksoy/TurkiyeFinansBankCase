c767e9d17b5da6794bdc287f12a60febfab5c6a5
Enes Aksoy <enesaksoy1168@gmail.com>
2022-02-26 02:19:19 +0300

    Added ObjectStore.

---
+++ TurkiyeFinansBankCase/ObjectStore/ObjectStore.swift
@@ -0,0 +1,15 @@
+//
+//  ObjectStore.swift
+//  TurkiyeFinansBankCase
+//
+//  Created by ENES AKSOY on 26.02.2022.
+//
+
+import Foundation
+
+class ObjectStore {
+    
+    static let shared = ObjectStore()
+    
+    var selectItem: SearchModel?
+}

e078dc1ef2a58d1111a8677b069be5d4369d5fd9
Enes Aksoy <enesaksoy1168@gmail.com>
2022-02-26 02:18:28 +0300

    Added WrapperType.

---
+++ TurkiyeFinansBankCase/Common/Enums/WrapperType.swift
@@ -0,0 +1,15 @@
+//
+//  WrapperType.swift
+//  TurkiyeFinansBankCase
+//
+//  Created by ENES AKSOY on 26.02.2022.
+//
+
+import Foundation
+
+enum WrapperType: String {
+    case movies = "movie"
+    case music = "musicVideo"
+    case apps = "software"
+    case books = "audiobook"
+}
